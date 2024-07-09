ebcf48877949fa6333d0bef9917460de0e609554
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-09 16:53:08 +0530

    projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Technologies from "@/components/Technologies";
 import Image from "next/image";
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Technologies />
+      <Projects />
     </main>
   );
 }
---
+++ src/components/Contact.tsx
@@ -0,0 +1,4 @@
+const Contact = () => {
+  return <div className='border-b border-neutral-800 pb-20'>Contact</div>;
+};
+export default Contact;
