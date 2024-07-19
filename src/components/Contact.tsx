81fefb080505fc264e18e0847fd1af48c7031395
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-19 21:19:54 +0530

    remove address tag

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div className='border-b border-neutral-800 pb-20'>
       <h1 className='my-10 text-center text-4xl'>Get in touch</h1>
       <div className='text-center tracking-tighter'>
-        <p className='my-4'>{CONTACT.address}</p>
+        {/* <p className='my-4'>{CONTACT.address}</p> */}
         {/* <p className='my-4'>{CONTACT.phoneNo}</p> */}
         <a
           href='#'
