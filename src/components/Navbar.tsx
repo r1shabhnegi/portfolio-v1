432cc2b7c296f6e565b82bd76e9962b8923db501
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-19 21:27:12 +0530

    navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className='mb-2 flex items-center justify-between py-6'>
@@ -15,10 +16,18 @@ const Navbar = () => {
         />
       </div>
       <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <FaLinkedin className='text-white' />
-        <FaGithub className='text-white' />
-        <FaSquareXTwitter className='text-white' />
-        <FaInstagram className='text-white' />
+        <Link href='https://www.linkedin.com/in/r1shabhnegi'>
+          <FaLinkedin className='text-white' />
+        </Link>
+        <Link href='https://github.com/r1shabhnegi'>
+          <FaGithub className='text-white' />
+        </Link>
+        <Link href='https://x.com/r1shabhnegi'>
+          <FaSquareXTwitter className='text-white' />
+        </Link>
+        <Link href='https://www.instagram.com/r1shabhnegi'>
+          <FaInstagram className='text-white' />
+        </Link>
       </div>
     </nav>
   );
