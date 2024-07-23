54264fba70f22d5f8ad398cabe45a6c37c73c07e
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-23 16:50:19 +0530

    link fixes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -38,7 +38,9 @@ const Projects = () => {
               initial={{ opacity: 0, x: 100 }}
               transition={{ duration: 1 }}
               className='w-full max-w-xl lg:w-3/4'>
-              <Link href={project.link}>
+              <Link
+                href={project.link}
+                target='_blank'>
                 <h6 className='mb-2 font-semibold'>{project.title}</h6>
               </Link>
               <p className='mb-4 text-neutral-400'>{project.description}</p>
